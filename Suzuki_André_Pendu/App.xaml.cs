f1d150da606d29fd8e86886814f36693fb37a5c9
SLAB74 <SLAB74@KM102-02>
2023-10-05 16:12:40 +0200

    Ajoutez des fichiers projet.

---
+++ Suzuki_André_Pendu/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Suzuki_André_Pendu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
